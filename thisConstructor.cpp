11dfe085d958ff8c33ba1c8783a57188440d8d1b
asus test <msa-99@outlook.com>
2024-05-21 23:49:32 +0600

    this and constructor function

---
+++ thisConstructor.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Student {
+    public:
+    int roll;
+    int clss;
+    double cgpa;
+    Student(int roll, int clss, int cgpa){
+        //this ekti pointer za oi class ke point kore rakhe. this ke derefrence korle *this dile oi class er property/variable access and assign kora zay.
+        this->roll=roll;
+        this->clss=clss;
+        this->cgpa=cgpa;
+    }
+};
+int main(){
+    Student msa(23, 9, 4.99);
+    cout<<msa.roll<<" "<<msa.clss<<" "<<msa.cgpa<<endl;
+    return 0;
+}
