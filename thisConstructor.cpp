db01ac0d6df194a1e23b4ad267bf5c030d9194d7
asus test <msa-99@outlook.com>
2024-05-21 23:51:17 +0600

    this and constructor function

--- thisConstructor.cpp
+++ thisConstructor.cpp
@@ -5,6 +5,7 @@ class Student {
     int roll;
     int clss;
     double cgpa;
+    //ekhane Student ekti constructor function. constructor function automatic call hoy.
     Student(int roll, int clss, int cgpa){
         //this ekti pointer za oi class ke point kore rakhe. this ke derefrence korle *this dile oi class er property/variable access and assign kora zay.
         this->roll=roll;
