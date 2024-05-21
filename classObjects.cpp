d0ab0381aa39acd336d1092a50d9c1963bc717d0
asus test <msa-99@outlook.com>
2024-05-21 22:54:02 +0600

    class declaration

--- classObjects.cpp
+++ classObjects.cpp
@@ -1,6 +1,11 @@
-#include<iostream>
-#include<iomanip>
+#include<bits/stdc++.h>
 using namespace std;
+class Student{
+    public:
+       char name[100];
+       int roll;
+       double cgpa;
+}
 int main(){
     
     return 0;
